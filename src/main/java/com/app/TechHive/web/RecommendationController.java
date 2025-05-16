b5dc207d02d7d46d87e1b1a28210062348ecc29b
Alexter2003 <alexvasuni@gmail.com>
2025-05-16 14:53:59 -0600

    feat: add controller to get Gemini Models and test responses to Gemini

---
+++ src/main/java/com/app/TechHive/web/RecommendationController.java
@@ -0,0 +1,29 @@
+package com.app.TechHive.web;
+
+import com.app.TechHive.domain.DTOs.ResourceResponse;
+import com.app.TechHive.domain.DTOs.UserRequest;
+import com.app.TechHive.domain.services.AI_service;
+import com.app.TechHive.domain.services.ResponseService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/recommendations")
+public class RecommendationController {
+    private ResponseService responseService;
+
+    public RecommendationController( ResponseService responseService) {
+        this.responseService = responseService;
+    }
+
+    @GetMapping("/models")
+    public ResponseEntity<String> getModels() {
+        return ResponseEntity.ok(responseService.getModels());
+    }
+
+    @PostMapping("/promptTest")
+    public ResponseEntity<ResourceResponse> getPromptTest(@RequestBody UserRequest request) throws JsonProcessingException {
+        return ResponseEntity.ok(responseService.getResources(request));
+    }
+}
